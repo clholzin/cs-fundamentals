54b3e554b5e85d036320c6270c6d0af8c72a4e52
Craig Holzinger <clholzin@gmail.com>
2020-11-14 20:51:24 -0700

    stack test

--- stack_test.go
+++ stack_test.go
@@ -12,8 +12,25 @@ func TestStack(t *testing.T) {
 		t.Errorf("expected 4")
 	}
 	stack.Push(223)
-
+	if val, _ := stack.Peak(); val != 223 {
+		t.Errorf("expected 223: got %d", val)
+	}
 	if err := stack.Pop(); err != nil {
 		t.Errorf("failed to pop")
+	}
+	if err := stack.Pop(); err != nil {
+		t.Errorf("failed to pop")
+	}
+
+	if _, err := stack.Peak(); err == nil {
+		t.Errorf("expected peak error")
+	}
+
+	if err := stack.Pop(); err == nil {
+		t.Errorf("expected pop error")
+	}
+
+	if !stack.IsEmpty() {
+		t.Errorf("!stack.IsEmpty()")
 	}
 }
