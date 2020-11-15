11e42ee829a3d093be7fc699ebb63d22ee3eed97
Craig Holzinger <clholzin@gmail.com>
2020-11-14 22:59:49 -0700

    MinIntHeap test

--- heap.go
+++ heap.go
@@ -1,12 +1,21 @@
 package fundamentals
 
+import (
+	"errors"
+	"fmt"
+)
+
 type MinIntHeap struct {
 	Cap   int
 	Size  int
 	items []int
 }
 
-func (heap *MinIntHeap) Items() []int {
+func NewMinIntHeap() *MinIntHeap {
+	return &MinIntHeap{Cap: 1}
+}
+
+func (heap *MinIntHeap) Values() []int {
 	return heap.items[:]
 }
 
@@ -46,15 +55,6 @@ func (heap *MinIntHeap) swap(indexOne, indexTwo int) {
 	heap.items[indexTwo] = temp
 }
 
-func (heap *MinIntHeap) ensureExtraCapacity() {
-	if heap.Size == heap.Cap {
-		temp := heap.items
-		heap.items = make([]int, heap.Cap*2)
-		heap.items = append(heap.items, temp...)
-		heap.Cap *= 2
-	}
-}
-
 func (heap *MinIntHeap) IsEmpty() bool {
 	return heap.Size == 0
 }
@@ -71,40 +71,53 @@ func (heap *MinIntHeap) Poll() (int, error) {
 		return 0, errors.New("empty")
 	}
 	item := heap.items[0]
-	items[0] = items[:heap.Size-1]
+	heap.items[0] = heap.items[heap.Size-1]
 	heap.Size--
-	heap.HeapifyDown()
+	heap.heapifyDown()
 	return item, nil
 }
 
-func (heap *MinIntHeap) Add(val int) {
-	heap.EnsureExtraCapacity()
-	heap.items[heap.Size] = val
-	heap.Size++
-	heap.HeapifyUp()
-}
-
-func (heap *MinIntHeap) HeapifyDown() {
-	index := 0
-	for heap.HasLeftChildIndex(index) {
-		smallerChildIndex := heap.GetLeftChildIndex(index)
-		if heap.HasRightChildIndex(index) && heap.RightChild(index) < heap.LeftChild(index) {
-			smallerChildIndex = heap.GetRightChildIndex[index]
-		}
-		if items[index] < items[smallerChildIndex] {
-			return
-		}
-		swap(index, smallerChildIndex)
-		index = smallrChildIndex
+func (heap *MinIntHeap) ensureExtraCapacity() {
+	if heap.items == nil {
+		heap.items = make([]int, 1)
+	}
+	if heap.Size == heap.Cap {
+		temp := heap.items[:]
+		heap.items = make([]int, heap.Cap*2)
+		heap.items = append(heap.items[:], temp...)
+		heap.Cap *= 2
 	}
 }
 
-func (heap *MinIntHeap) HeapifyUp() {
+func (heap *MinIntHeap) Add(val int) {
+	heap.ensureExtraCapacity()
+	fmt.Println(heap.items, heap.Size, val)
+	heap.items[heap.Size] = val
+	heap.Size++
+	heap.heapifyUp()
+}
+
+func (heap *MinIntHeap) heapifyDown() {
+	index := 0
+	for heap.HasLeftChild(index) {
+		smallerChildIndex := heap.GetLeftChildIndex(index)
+		if heap.HasRightChild(index) && heap.RightChild(index) < heap.LeftChild(index) {
+			smallerChildIndex = heap.GetRightChildIndex(index)
+		}
+		if heap.items[index] < heap.items[smallerChildIndex] {
+			return
+		}
+		heap.swap(index, smallerChildIndex)
+		index = smallerChildIndex
+	}
+}
+
+func (heap *MinIntHeap) heapifyUp() {
 	index := heap.Size - 1
-	predicate := (heap.HasParent(index) && heap.Parent(index) > items[index])
+	predicate := (heap.HasParent(index) && heap.Parent(index) > heap.items[index])
 	for predicate {
 		heap.swap(heap.GetParentIndex(index), index)
 		index = heap.GetParentIndex(index)
-		predicate = (heap.HasParent(index) && heap.Parent(index) > items[index])
+		predicate = (heap.HasParent(index) && heap.Parent(index) > heap.items[index])
 	}
 }
--- heap_test.go
+++ heap_test.go
@@ -1,1 +1,28 @@
 package fundamentals
+
+import "testing"
+
+func TestMinIntHeap(t *testing.T) {
+
+	heap := NewMinIntHeap()
+	heap.Add(10)
+	heap.Add(12)
+	heap.Add(14)
+	heap.Add(22)
+	heap.Add(24)
+	heap.Add(32)
+	heap.Add(34)
+
+	if heap.Size != 7 {
+		t.Errorf("size is not 7: %v", heap.Values())
+	}
+
+	if val, err := heap.Peak(); err == nil {
+		if val != 10 {
+			t.Errorf("peak value expected 10. was: %d", val)
+		}
+	} else {
+		t.Errorf("peak failed")
+	}
+
+}
