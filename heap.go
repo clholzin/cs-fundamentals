e82eeb971e6aa7697d9903a72488c7a7acd00a44
Craig Holzinger <clholzin@gmail.com>
2020-12-03 20:28:19 -0700

    revisit heaps 2*parentindex +1 2*parentindex +2 (childindex -1) / 2

--- heap.go
+++ heap.go
@@ -6,119 +6,126 @@ import (
 )
 
 type MinIntHeap struct {
-	Cap   int
-	Size  int
+	cap   int
+	size  int
 	items []int
 }
 
 func NewMinIntHeap() *MinIntHeap {
-	return &MinIntHeap{Cap: 10}
+	return &MinIntHeap{cap: 10}
 }
 
-func (heap *MinIntHeap) Items() []int {
-	return heap.items[:]
+func (h *MinIntHeap) Items() []int {
+	return h.items[:]
 }
 
-func (heap *MinIntHeap) GetLeftChildIndex(parentIndex int) int {
+func (h *MinIntHeap) Size() int {
+	return h.size
+}
+
+func (h *MinIntHeap) GetLeftChildIndex(parentIndex int) int {
 	return 2*parentIndex + 1
 }
-func (heap *MinIntHeap) GetRightChildIndex(parentIndex int) int {
+func (h *MinIntHeap) GetRightChildIndex(parentIndex int) int {
 	return 2*parentIndex + 2
 }
-func (heap *MinIntHeap) GetParentIndex(childIndex int) int {
+func (h *MinIntHeap) GetParentIndex(childIndex int) int {
 	return (childIndex - 1) / 2
 }
 
-func (heap *MinIntHeap) HasLeftChild(index int) bool {
-	return heap.GetLeftChildIndex(index) < heap.Size
+func (h *MinIntHeap) HasLeftChild(index int) bool {
+	return h.GetLeftChildIndex(index) < h.size
 }
-func (heap *MinIntHeap) HasRightChild(index int) bool {
-	return heap.GetRightChildIndex(index) < heap.Size
+func (h *MinIntHeap) HasRightChild(index int) bool {
+	return h.GetRightChildIndex(index) < h.size
 }
-func (heap *MinIntHeap) HasParent(index int) bool {
-	return heap.GetParentIndex(index) >= 0
-}
-
-func (heap *MinIntHeap) LeftChild(index int) int {
-	return heap.items[heap.GetLeftChildIndex(index)]
-}
-func (heap *MinIntHeap) RightChild(index int) int {
-	return heap.items[heap.GetRightChildIndex(index)]
-}
-func (heap *MinIntHeap) Parent(index int) int {
-	return heap.items[heap.GetParentIndex(index)]
+func (h *MinIntHeap) HasParent(index int) bool {
+	return h.GetParentIndex(index) >= 0
 }
 
-func (heap *MinIntHeap) swap(indexOne, indexTwo int) {
-	temp := heap.items[indexOne]
-	heap.items[indexOne] = heap.items[indexTwo]
-	heap.items[indexTwo] = temp
+func (h *MinIntHeap) LeftChild(index int) int {
+	return h.items[h.GetLeftChildIndex(index)]
+}
+func (h *MinIntHeap) RightChild(index int) int {
+	return h.items[h.GetRightChildIndex(index)]
+}
+func (h *MinIntHeap) Parent(index int) int {
+	return h.items[h.GetParentIndex(index)]
 }
 
-func (heap *MinIntHeap) IsEmpty() bool {
-	return heap.Size == 0
+func (h *MinIntHeap) swap(indexOne, indexTwo int) {
+	temp := h.items[indexOne]
+	h.items[indexOne] = h.items[indexTwo]
+	h.items[indexTwo] = temp
 }
 
-func (heap *MinIntHeap) Peak() (int, error) {
-	if heap.IsEmpty() {
+func (h *MinIntHeap) IsEmpty() bool {
+	return h.size == 0
+}
+
+func (h *MinIntHeap) Peak() (int, error) {
+	if h.IsEmpty() {
 		return 0, errors.New("empty")
 	}
-	return heap.items[0], nil
+	return h.items[0], nil
 }
 
-func (heap *MinIntHeap) Poll() (int, error) {
-	if heap.IsEmpty() {
+func (h *MinIntHeap) Poll() (int, error) {
+	if h.IsEmpty() {
 		return 0, errors.New("empty")
 	}
-	item := heap.items[0]
-	heap.items[0] = heap.items[heap.Size-1]
-	heap.Size--
-	heap.heapifyDown()
-	fmt.Println("-", heap.items, heap.Size, heap.Cap)
+	item := h.items[0]
+	h.items[0] = h.items[h.size-1]
+	h.items[h.size-1] = 0
+	h.size--
+	h.heapifyDown()
+	fmt.Println("-", h.items, h.size, h.cap)
 	return item, nil
 }
 
-func (heap *MinIntHeap) ensureExtraCapacity() {
-	if heap.items == nil {
-		heap.items = make([]int, heap.Cap)
+func (h *MinIntHeap) ensureExtracapacity() {
+	if h.items == nil {
+		h.items = make([]int, h.cap)
 	}
-	if heap.Size == heap.Cap {
-		temp := heap.items[:]
-		heap.items = make([]int, heap.Cap*2)
-		heap.items = append(heap.items[:], temp...)
-		heap.Cap *= 2
+	if h.size == h.cap {
+		temp := h.items[:]
+		h.items = make([]int, h.cap*2)
+		h.items = append(h.items[:], temp...)
+		h.cap *= 2
 	}
 }
 
-func (heap *MinIntHeap) Add(val int) {
-	heap.ensureExtraCapacity()
-	heap.items[heap.Size] = val
-	heap.Size++
-	heap.heapifyUp()
-	fmt.Println("+", heap.items, heap.Size, heap.Cap, val)
+func (h *MinIntHeap) Add(val int) {
+	h.ensureExtracapacity()
+	h.items[h.size] = val
+	h.size++
+	h.heapifyUp()
+	fmt.Println("+", h.items, h.size, h.cap, val)
 }
 
-func (heap *MinIntHeap) heapifyDown() {
+func (h *MinIntHeap) heapifyDown() {
 	index := 0
-	for heap.HasLeftChild(index) {
-		smallerChildIndex := heap.GetLeftChildIndex(index)
-		if heap.HasRightChild(index) && heap.RightChild(index) > heap.LeftChild(index) {
-			smallerChildIndex = heap.GetRightChildIndex(index)
+	for h.HasLeftChild(index) {
+		smallerChildIndex := h.GetLeftChildIndex(index)
+		//fmt.Println("[]", h.items, h.size, h.cap, smallerChildIndex)
+
+		if h.HasRightChild(index) && h.RightChild(index) < h.LeftChild(index) {
+			smallerChildIndex = h.GetRightChildIndex(index)
 		}
-		if heap.items[index] < heap.items[smallerChildIndex] {
-			return
+		if h.items[index] < h.items[smallerChildIndex] {
+			break
+		} else {
+			h.swap(index, smallerChildIndex)
 		}
-		heap.swap(index, smallerChildIndex)
 		index = smallerChildIndex
 	}
 }
 
-func (heap *MinIntHeap) heapifyUp() {
-	index := heap.Size - 1
-	predicate := heap.HasParent(index) && (heap.Parent(index) > heap.items[index])
-	for predicate {
-		heap.swap(heap.GetParentIndex(index), index)
-		index = heap.GetParentIndex(index)
-		predicate = heap.HasParent(index) && (heap.Parent(index) > heap.items[index])
+func (h *MinIntHeap) heapifyUp() {
+	index := h.size - 1
+	for h.HasParent(index) && (h.Parent(index) > h.items[index]) {
+		h.swap(h.GetParentIndex(index), index)
+		index = h.GetParentIndex(index)
+		//fmt.Println("^", h.items, h.size, h.cap, index)
 	}
 }
--- heap_test.go
+++ heap_test.go
@@ -5,26 +5,42 @@ import "testing"
 func TestMinIntHeap(t *testing.T) {
 
 	heap := NewMinIntHeap()
+	heap.Add(24)
+	heap.Add(32)
+	heap.Add(34)
 	heap.Add(10)
 	heap.Add(12)
 	heap.Add(14)
 	heap.Add(22)
-	heap.Add(24)
-	heap.Add(32)
-	heap.Add(34)
 
-	if heap.Size != 7 {
+	if heap.Size() != 7 {
 		t.Errorf("size is not 7: %v", heap.Items())
 	}
-
-	heap.Poll()
-
-	if val, err := heap.Peak(); err == nil {
-		if val != 14 {
+	val, err := heap.Poll()
+	if err != nil {
+		t.Errorf("peak failed %d - %s", val, err)
+	}
+	//t.Logf("first poll %d", val)
+	val, err = heap.Peak()
+	if err == nil {
+		if val != 12 {
 			t.Errorf("peak value expected 14. was: %d", val)
 		}
 	} else {
 		t.Errorf("peak failed")
+	}
+
+	var pre int
+	for !heap.IsEmpty() {
+		if val, err := heap.Poll(); err == nil {
+			if pre > val {
+				t.Errorf("%d > %d", pre, val)
+				break
+			}
+			t.Logf("%d", val)
+			pre = val
+		}
+
 	}
 
 }
