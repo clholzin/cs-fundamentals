290c271e6a60994fbe43e3b95671af074ea53e53
Craig Holzinger <craigholzinger@Holzinger-MacBook-Pro.local>
2021-09-04 16:10:25 -0700

    alternate type

--- trapping_rain_water.go
+++ trapping_rain_water.go
@@ -29,6 +29,43 @@ func trap(height []int) int {
 }
 
 /*
+func lavaVolumeSum(arr []int) int {
+	var sum int
+	if len(arr) == 0 {
+		return sum
+	}
+	for i := 1; i < len(arr)-1; i++ {
+		leftIdx, rightIdx := i, i
+
+		for leftIdx > 0 {
+			if arr[i] < arr[leftIdx] {
+				break
+			}
+			leftIdx--
+		}
+
+		for rightIdx < len(arr) {
+			if arr[i] < arr[rightIdx] || rightIdx == (len(arr)-1) {
+				break
+			}
+			rightIdx++
+		}
+
+		minSideLevel := minSide(arr[leftIdx], arr[rightIdx])
+		sum += int(math.Abs(float64(minSideLevel) - float64(arr[i])))
+	}
+	return sum
+}
+
+func minSide(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+*/
+
+/*
 func trap(height []int) int {
 	if len(height) == 0 {
 		return int(0)
